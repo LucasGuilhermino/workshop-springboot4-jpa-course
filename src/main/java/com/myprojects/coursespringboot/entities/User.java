ffbc5a00bb9caac87fef67d96c5efa3cfef13d63
Lucas Guilhermino <lucasguilherminooliveira@gmail.com>
2026-04-14 12:28:38 -0300

    H2 database, test profile, JPA

--- src/main/java/com/myprojects/coursespringboot/entities/User.java
+++ src/main/java/com/myprojects/coursespringboot/entities/User.java
@@ -3,11 +3,23 @@ package com.myprojects.coursespringboot.entities;
 import java.io.Serializable; //objetos transformados em sequencias de bytes para trafegar na rede
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity // para falar que será uma entidade no banco de dados
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id // para falar que é id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // para dizer que é auto incrementável
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
