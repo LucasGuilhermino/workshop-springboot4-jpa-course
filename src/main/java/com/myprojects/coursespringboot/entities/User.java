7775f674cf77286c36f8f9f9a049c1b5c56f8f5f
Lucas Guilhermino <lucasguilherminooliveira@gmail.com>
2026-04-14 14:10:18 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/myprojects/coursespringboot/entities/User.java
+++ src/main/java/com/myprojects/coursespringboot/entities/User.java
@@ -1,17 +1,20 @@
 package com.myprojects.coursespringboot.entities;
 
 import java.io.Serializable; //objetos transformados em sequencias de bytes para trafegar na rede
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
 @Entity // para falar que será uma entidade no banco de dados
-@Table(name = "tb_user")
+@Table(name = "tb_user") // nomeia a tabela no db
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -24,6 +27,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 
@@ -73,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders(){
+		return orders;
 	}
 
 	@Override
